5049eea92d9dae6cad9d32c109e12d240adb14c9
Dzu Nguyen <dustinwind@gmail.com>
2018-05-29 23:46:41 -0400

    add dzu layout

--- keyboards/whitefox/config.h
+++ keyboards/whitefox/config.h
@@ -77,4 +77,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
+
+/* Mouse button configuration */
+#define MOUSEKEY_DELAY             40
+#define MOUSEKEY_INTERVAL          20
+#define MOUSEKEY_MAX_SPEED         5
+#define MOUSEKEY_TIME_TO_MAX       30
+#define MOUSEKEY_WHEEL_MAX_SPEED   8
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40
+
 #endif
