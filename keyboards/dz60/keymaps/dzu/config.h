a69893f130fa9a9591ef9881a901896e698ba610
Dzu Nguyen <dustinwind@gmail.com>
2018-06-05 00:08:29 -0400

    custom dz60 layout

---
+++ keyboards/dz60/keymaps/dzu/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_KEYMAP_H
+#define CONFIG_KEYMAP_H
+
+#include "../../config.h"
+
+// help for fast typist+dual function keys?
+#define PERMISSIVE_HOLD
+
+/* Mouse button configuration */
+#define MOUSEKEY_DELAY             20
+#define MOUSEKEY_INTERVAL          15
+#define MOUSEKEY_MAX_SPEED         7
+#define MOUSEKEY_TIME_TO_MAX       30
+#define MOUSEKEY_WHEEL_INTERVAL		5
+#define MOUSEKEY_WHEEL_MAX_SPEED   4
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 200
+
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_EFFECT_CHRISTMAS_INTERVAL 300
+#define RGBLIGHT_EFFECT_KNIGHT_LED_NUM 16
+
+#endif
