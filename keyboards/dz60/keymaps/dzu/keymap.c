d3b1ccf4696d4118ee111d1284d63b5408b3c714
DESKTOP-OTK3RG8\dustinwind <dustinwind@gmail.com>
2018-12-01 23:57:30 -0500

    capslock on -> LED on capslock off -> LED off

--- keyboards/dz60/keymaps/dzu/keymap.c
+++ keyboards/dz60/keymaps/dzu/keymap.c
@@ -203,12 +203,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         set_single_persistent_default_layer(_MAC);
       }
       return false;
-	case KC_CAPS:
-		if (record->event.pressed) {
-			rgblight_toggle();
-			rgblight_setrgb_turquoise();
-		}
-		return true; // allow capslock to be processed as normal
+	// case KC_CAPS:
+	// 	if (record->event.pressed) {
+	// 		rgblight_toggle();
+	// 		rgblight_setrgb_turquoise();
+	// 	}
+	// 	return true; // allow capslock to be processed as normal
   }
   return true;
 }
@@ -222,9 +222,10 @@ void led_set_user(uint8_t usb_led) {
 	}
 
 	if (usb_led & (1 << USB_LED_CAPS_LOCK)) {
-		
+		rgblight_enable();
+		rgblight_setrgb_turquoise();
 	} else {
-		
+		rgblight_disable();
 	}
 
 	if (usb_led & (1 << USB_LED_SCROLL_LOCK)) {
