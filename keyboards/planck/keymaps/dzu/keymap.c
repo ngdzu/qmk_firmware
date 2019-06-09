c6f74b58a98f86c9a6a761e4b0fd94e171446d9e
Dzu Nguyen <dzu.nguyen@avid.com>
2019-06-08 23:54:28 -0400

    Add debug macros

--- keyboards/planck/keymaps/dzu/keymap.c
+++ keyboards/planck/keymaps/dzu/keymap.c
@@ -7,18 +7,60 @@
 
 // Fillers to make layering more clear
 #define XXXXXXX KC_NO
-#define _______ KC_TRNS
+#define ________ KC_TRNS
+#define LT_FN2 LT(_FN2, ________)
+
 
 
 #define _QWERTY 0
 #define _LOWER  1
 #define _RAISE  2
 #define _FUNC   3
+#define _FN1    4
+#define _FN2    5
+
 
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define FUNCT MO(_FUNC)
+#define FN1    MO(_FN1)
+#define FN2    MO(_FN2)
+
+// macros
+#define M_MOVE_NEXT_LINE 	0
+#define M_MOVE_PREV_LINE 	1
+#define M_STEP_OUT 		  	2
+#define M_MOVE_CUR_LINE 	3
+#define M_FIND 				    4
+#define M_HOME 			      5
+#define M_END 				    6
+
+#define M_CONTINUE_MAC		100
+
+#define FIND M(M_FIND)
+
+
+#define M_MNL M_MOVE_NEXT_LINE
+#define M_MPL M_MOVE_PREV_LINE
+#define M_STO M_STEP_OUT
+#define M_MCL M_MOVE_CUR_LINE
+
+#define M_C_MAC M_CONTINUE_MAC
+
+#define KC_STEP_OVER KC_F10
+#define KC_STEP_IN KC_F11
+#define CONT KC_F5
+
+#define STEP_OV KC_STEP_OVER
+#define STEP_IN KC_STEP_IN
+#define STEP_OUT M(M_STO)
+
+#define STEP_OV_MAC KC_F6
+#define STEP_IN_MAC KC_F7
+#define STEP_OUT_MAC KC_F8
+#define CONT_MAC M(M_C_MAC)
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -31,14 +73,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |RCtrl |RShift|
+ * | Ctrl | Alt  | GUI  | Fn1   |Lower|    Space    |Raise | Fn2 | Down  |RCtrl |RShift|
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-  FUNCT ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-  KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_RCTL, KC_RSFT
+  KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC ,
+  FUNCT   , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,
+  KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT  ,
+  KC_LCTL , KC_LALT , KC_LGUI , FN1     , LOWER   , KC_SPC  , KC_SPC  , RAISE   , ________, ________, KC_RCTL , KC_RSFT
 ),
 
 /* Lower
@@ -53,10 +95,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______,
-  RESET,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN   , KC_RPRN , KC_BSPC ,
+  KC_DEL  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_UNDS , KC_PLUS , KC_LCBR   , KC_RCBR , KC_PIPE ,
+  ________, KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , S(KC_NUHS),S(KC_NUBS),________, ________, ________,
+  RESET   , ________, ________, ________, ________, ________, ________, ________, KC_MNXT , KC_VOLD   , KC_VOLU , KC_MPLY
 ),
 
 /* Raise
@@ -71,10 +113,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______,
-  RESET,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_GRV  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    ,KC_BSPC  ,
+  KC_DEL  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_MINS , KC_EQL  , KC_LBRC , KC_RBRC , KC_BSLS ,
+  ________, KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_NUHS , KC_NUBS , ________, ________, ________,
+  RESET   , ________, ________, ________, ________, ________, ________, ________, KC_MNXT , KC_VOLD , KC_VOLU , KC_MPLY
 ),
 
 
@@ -99,14 +141,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
  * | _____  | _____ | _____ | _____ | _____ | _____ | _____ | _____ | _____ | _____ | _____ | _____ |
  * `------------------------------------------------------------------------------------------------'
- */
+ */ 
 
 [_FUNC] = LAYOUT_planck_grid(
-  _______, _______, _______, KC_MS_U , _______, _______, _______, _______, KC_UP, _______, _______, _______,
-  _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_BTN1,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  ________, ________, ________, KC_MS_U , ________, ________, ________, ________, KC_UP   , ________, ________, ________,
+  ________, LT_FN2  , KC_MS_L , KC_MS_D , KC_MS_R , ________, ________, KC_LEFT , KC_DOWN , KC_RGHT , ________, KC_BTN1 ,
+  ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________,
+  ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________
+),
+
+[_FN1] = LAYOUT_planck_grid(
+  ________, ________, ________, ________, ________, ________, ________, KC_7    , KC_8    , KC_9    , KC_0    , ________,
+  ________, ________, ________, ________, ________, ________, ________, KC_4    , KC_5    , KC_6    , ________, ________,
+  ________, ________, ________, ________, ________, ________, ________, KC_1    , KC_2    , KC_3    , ________, ________,
+  ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________
+),
+
+[_FN2] = LAYOUT_planck_grid(
+  ________, ________, ________, ________, ________, ________, ________, KC_F7   , KC_F8   , KC_F9   , KC_F10  , ________,
+  ________, ________, ________, ________, FIND    , ________, ________, KC_F4   , KC_F5   , KC_F6   , KC_F11  , ________,
+  ________, ________, ________, ________, ________, ________, ________, KC_F1   , KC_F2   , KC_F3   , KC_F12  , ________,
+  ________, ________, ________, ________, ________, ________, ________, ________, STEP_OV , STEP_IN , STEP_OUT, CONT
 )
+
 
 };
 
@@ -114,23 +171,49 @@ const uint16_t PROGMEM fn_actions[] = {
 
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  switch(id) {
-    case 1:
-      if (record->event.pressed) {
-        layer_on(1);
-      } else {
-        layer_off(1);
-      }
-      break;
-    case 2:
-      if (record->event.pressed) {
-        layer_on(2);
-      } else {
-        layer_off(2);
-      }
-      break;
-  }
-  return MACRO_NONE;
-};
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+	// keyevent_t event = record->event; //unused
+
+	switch (id) {
+		case M_MOVE_NEXT_LINE:
+			if (record->event.pressed) {
+				return MACRO( T(DOWN), D(LCTL), D(LSFT), T(F10), U(LSFT), U(LCTL), END );
+			}
+			break;
+		case M_MOVE_PREV_LINE:
+			if (record->event.pressed) {
+				return MACRO( T(UP), D(LCTL), D(LSFT), T(F10), U(LSFT), U(LCTL), END );
+			}
+			break;
+		case M_STEP_OUT:
+			if (record->event.pressed) {
+				return MACRO( D(LSFT), T(F11), U(LSFT), END );
+			}
+			break;
+		case M_MOVE_CUR_LINE:
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), D(LSFT), T(F10), U(LSFT), U(LCTL), END );
+			}
+		case M_FIND:
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), T(F3), U(LCTL), END );
+			}
+			break;
+		case M_HOME:
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), T(HOME), U(LCTL), END );
+			}
+			break;
+		case M_END:
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), T(END), U(LCTL), END );
+			}
+			break;
+		case M_CONTINUE_MAC:
+			if (record->event.pressed) {
+				return MACRO( D(LCTL), D(LALT),T(Y), U(LALT),U(LCTL), END );
+			}
+			break;
+	}
+	return MACRO_NONE;
+}
