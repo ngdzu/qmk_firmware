bb21a31b4206d2540275974244f0d3b062e312e6
Dzu Nguyen <dzu.nguyen@avid.com>
2019-06-15 18:10:30 -0400

    move del key

--- keyboards/planck/keymaps/dzu/keymap.c
+++ keyboards/planck/keymaps/dzu/keymap.c
@@ -107,20 +107,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | FN1  | Alt  | GUI  | LOWER|Ctrl  |    Space    |Ctrl  | RAISE|      |      | Alt  |
+ * | FN1  | Alt  | GUI  | LOWER|Ctrl  |    Space    |Ctrl  | RAISE|      |      | Del  |
  * `-----------------------------------------------------------------------------------'
  */
     [_QWERTY] = LAYOUT_planck_grid(
         KC_TAB  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC ,
         FUNCT   , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,
         KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT  ,
-        FN1     , KC_LGUI , KC_LALT , KC_LCTL , LOWER   , KC_SPC  , KC_SPC  , RAISE   , KC_RCTL , ________, ________, KC_LALT),
+        FN1     , KC_LGUI , KC_LALT , KC_LCTL , LOWER   , KC_SPC  , KC_SPC  , RAISE   , KC_RCTL , ________, ________, KC_DEL  ),
 
     /* Lower
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |   |  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -129,7 +129,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_LOWER] = LAYOUT_planck_grid(
         KC_TILD , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , KC_BSPC ,
-        KC_DEL  , ________, KC_LBRC , KC_RBRC , ________, ________, ________, ________, KC_UNDS , KC_PLUS , ________, ________,
+        ________, ________, KC_LBRC , KC_RBRC , ________, ________, ________, ________, KC_UNDS , KC_PLUS , ________, ________,
         ________, ________, ________, ________, ________, ________, ________, KC_PIPE , KC_LT   , KC_GT   , KC_QUES , ________,
         RESET   , ________, ________, ________, ________, ________, ________, ________, ________, KC_VOLD , KC_VOLU , KC_MPLY),
 
